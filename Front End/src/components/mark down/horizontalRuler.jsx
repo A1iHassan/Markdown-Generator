c39af27ccce61cd1c3dab5603351fe42614e2273
A1iHassan <ali012wkout@gmail.com>
2024-07-06 22:20:53 +0300

    a commit by Ali Hassan

--- Front End/src/components/mark down/horizontalRuler.jsx
+++ Front End/src/components/mark down/horizontalRuler.jsx
@@ -2,11 +2,13 @@ import { horizontalRule } from "../../constants/markdownElements";
 import { useContext, useEffect } from "react";
 import { AST } from "../../util/contextProvidor";
 
-export default function HorizontalRuleElement() {
+export default function HorizontalRuleElement({ ID }) {
   const { ast, setAst } = useContext(AST);
   useEffect(() => {
     const newAst = { ...ast };
-    newAst.children.push(horizontalRule());
+    const newNode = horizontalRule();
+    newNode.id = ID;
+    newAst.children.push(newNode);
     setAst(newAst);
   }, []);
   return <hr />;
