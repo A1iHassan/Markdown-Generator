8a4b3cfbc55c51caa97b4f2b0abe82f6f0dbdc45
A1iHassan <ali012wkout@gmail.com>
2025-02-17 01:23:01 +0200

    added the text boxes where you would enter text in one, and it will appear in the other

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,23 @@
+import { ChangeEvent, useState } from "react";
+
 function App() {
+  const [markdown, setMarkdown] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(e.target.value);
+  };
   return (
-    <>
-      <span className="text-4xl bg-red-400 rounded m-5 p-2 text-slate-50">
-        hello there
-      </span>
-    </>
+    <div className="flex flex-row justify-around h-screen">
+      <p className="m-3 bg-amber-50 focus:outline-amber-200 flex-1 h-1/2 resize-none p-2">
+        {markdown}
+      </p>
+      <textarea
+        className="m-3 bg-amber-50 focus:outline-amber-200 flex-1 h-1/2 resize-none p-2 font-mono"
+        name="result"
+        id="result"
+        onChange={handleChange}
+      ></textarea>
+    </div>
   );
 }
 
